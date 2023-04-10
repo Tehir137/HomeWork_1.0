55d571bc23f619cc0cc0ef79c478040d4764d0ee
Vladislav Berezskii <berezskiss137@gmail.com>
2023-04-10 21:59:41 +0300

    Я сделал

---
+++ Dorabotka.py
@@ -0,0 +1,16 @@
+import pytest
+from selene.support.shared import browser
+from selene import be, have
+
+
+@pytest.fixture()
+def brow():
+    browser.open('https://google.com')
+    browser.driver.set_window_size(3200, 2000)
+    yield
+    browser.close()
+
+
+def test_size_brow(brow):
+    browser.element('[name="q"]').should(be.blank).set_value('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))

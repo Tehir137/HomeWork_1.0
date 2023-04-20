cf4aeaa0d4d24266f6eb127c9866193881f066fe
Vladislav Berezskii <berezskiss137@gmail.com>
2023-04-20 14:19:23 +0300

    Доработал

--- Dorabotka.py
+++ Dorabotka.py
@@ -14,3 +14,9 @@ def brow():
 def test_size_brow(brow):
     browser.element('[name="q"]').should(be.blank).set_value('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_search(brow):
+    browser.element('[name="q"]').should(be.blank).set_value('sfdahansdnsdfgnsr').press_enter()
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
+    print('По результату теста Search ничего не найдено')
